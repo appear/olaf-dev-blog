dca5b2860db6bad5f3b46c877d8476d70bebcca4
olaf <appear.ko@gmail.com>
2020-09-16 21:09:30 +0900

    feature: project setup

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,10 +1,12 @@
-/**
- * Configure your Gatsby site with this file.
- *
- * See: https://www.gatsbyjs.org/docs/gatsby-config/
- */
-
 module.exports = {
-  /* Your site config here */
-  plugins: [],
+  plugins: [
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `markdown-pages`,
+        path: `${__dirname}/data/pages`,
+      },
+    },
+    `gatsby-transformer-remark`,
+  ],
 }
