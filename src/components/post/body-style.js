6c9fb110fa80cfd6566db3610d051157d5f870bc
olaf <olaf@olafui-MacBookPro.local>
2020-09-26 16:34:50 +0900

    feature: element 요소들의 간격을 조절합니다

--- src/components/post/body-style.js
+++ src/components/post/body-style.js
@@ -1,20 +1,22 @@
 export default `
   font-size: 16px;
   line-height: 1.63;
-  color: rgba(58, 58, 58, 0.9);
+  color: #444;
 
   h1 {
-    padding: 20px 0;
+    margin: 20px 0;
     font-size: 24px;
     font-weight: bold;
   }
+
   h2 {
-    padding: 20px 0;
+    margin: 20px 0;
     font-size: 21px;
     font-weight: bold;
   }
+
   h3 {
-    padding: 10px 0;
+    margin: 10px 0;
     font-size: 19px;
     font-weight: bold;
   }
@@ -23,41 +25,50 @@ export default `
     font-size: 16px;
     font-weight: bold;
   }
+
   blockquote {
     padding: 15px;
     font-size: 14px;
     color: rgba(58, 58, 58, 0.8);
     background: #fafafa;
-    border-radius: 5px;
+    border-radius: 8px;
   }
+
   blockquote > p {
     padding: 0;
   }
+
   a {
     color: hotpink;
     font-weight: bold;
   }
+  
   p {
     padding: 10px 0;
   }
+
   ul {
-    padding: 20px 0;
+    margin: 20px 0;
   }
+
   li {
     position: relative;
-    padding: 10px 0 10px 15px;
+    padding: 10px 0 10px 25px;
   }
+
   li:before {
-    content: "·";
+    content: "👉";
     position: absolute;
     left: 0;
-    top: 50%;
-    transform: translateY(-50%);
+    top: 10px;
     font-weight: bold;
   }
+
   img {
     width: 100%;
+    border-radius: 8px;
   }
+
   hr {
     margin: 50px 0;
     height: 1px;
